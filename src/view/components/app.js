eed44b88046c14ab5dbb01d85658d255d927b5d2
IntPtr <lxy.lixiaoyan@gmail.com>
2016-03-10 20:48:02 +0800

    update stateless components' code style

--- src/view/components/app.js
+++ src/view/components/app.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-export default (props) => (
+const App = (props) => (
   <div>
     {props.children}
   </div>
 );
+
+export default App;
--- src/view/components/home.js
+++ src/view/components/home.js
@@ -1,5 +1,7 @@
 import React from "react";
 
-export default () => (
+const Home = () => (
   <main></main>
 );
+
+export default Home;
